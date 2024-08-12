b0a1fbb7367e5be71704cf42fea5e9dd112f6eee
tdddt <doto1116@ewhain.net>
2024-08-12 14:59:08 +0900

    :tada: initialize project

---
+++ src/main/java/com/capstone03/goldenglobe/GoldenglobeApplication.java
@@ -0,0 +1,13 @@
+package com.capstone03.goldenglobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenglobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenglobeApplication.class, args);
+	}
+
+}
