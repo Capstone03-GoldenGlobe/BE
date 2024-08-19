c3d0dca9dc83fbb8e8f487383b50cebe075b658e
tdddt <doto1116@ewhain.net>
2024-08-19 16:33:35 +0900

    :lock: : SecurityConfig 파일 추가

---
+++ src/main/java/com/capstone03/goldenglobe/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.capstone03.goldenglobe;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CsrfTokenRepository csrfTokenRepository(){
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((crsf) -> crsf.disable());
+
+        http.authorizeHttpRequests((authorize) ->
+                authorize.requestMatchers("/**").permitAll() // 전체 URI에 대해 허용(로그인 필요x)
+        );
+
+        return http.build();
+    }
+}
