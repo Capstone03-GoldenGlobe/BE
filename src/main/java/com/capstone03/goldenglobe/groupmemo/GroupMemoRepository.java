829197c74758ff55c05e1b416a99dc2dfbb3896a
tdddt <doto1116@ewhain.net>
2024-08-19 19:08:57 +0900

    :sparkles: : 체크리스트 전체 조회 API

--- src/main/java/com/capstone03/goldenglobe/groupmemo/GroupMemoRepository.java
+++ src/main/java/com/capstone03/goldenglobe/groupmemo/GroupMemoRepository.java
@@ -1,8 +1,13 @@
 package com.capstone03.goldenglobe.groupmemo;
 
+import com.capstone03.goldenglobe.listgroup.ListGroup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface GroupMemoRepository extends JpaRepository<GroupMemo, Long> {
+    Optional<GroupMemo> findByGroup_GroupId(Long listGroupId);
 }
--- src/main/java/com/capstone03/goldenglobe/listgroup/ListGroupRepository.java
+++ src/main/java/com/capstone03/goldenglobe/listgroup/ListGroupRepository.java
@@ -2,5 +2,8 @@ package com.capstone03.goldenglobe.listgroup;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ListGroupRepository extends JpaRepository<ListGroup, Long> {
+    List<ListGroup> findByList_ListId(Long checkListId);
 }
