477f6464118d1039bc26a4e7709eb00554904ae0
tdddt <doto1116@ewhain.net>
2024-08-20 16:35:54 +0900

    :sparkles: : 체크리스트 메모 삭제 API

--- src/main/java/com/capstone03/goldenglobe/groupMemo/GroupMemoController.java
+++ src/main/java/com/capstone03/goldenglobe/groupMemo/GroupMemoController.java
@@ -4,10 +4,7 @@ package com.capstone03.goldenglobe.groupMemo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +14,7 @@ import java.util.Map;
 public class GroupMemoController {
 
     private final GroupMemoService groupMemoService;
+    private final GroupMemoRepository groupMemoRepository;
     @PostMapping("/checklists/{group_id}/memos")
     public ResponseEntity<Map<String, Object>> postMemo(@PathVariable Long group_id, @RequestParam String memo) {
         GroupMemo groupMemo = groupMemoService.makeMemo(group_id,memo);
@@ -36,6 +34,17 @@ public class GroupMemoController {
         Map<String, Object> response = new HashMap<>();
         response.put("status", 200);
         response.put("message", "항목 변경 완료");
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/checklists/memos")
+    public ResponseEntity<Map<String, Object>> deleteItem(@RequestParam Long memo_id) {
+        groupMemoRepository.deleteById(memo_id);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", 200);
+        response.put("message", "메모 삭제 완료");
 
         return ResponseEntity.ok(response);
     }
