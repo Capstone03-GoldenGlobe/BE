03bcd12600757c601561ff4149f1d1f983d5ff4d
tdddt <doto1116@ewhain.net>
2024-09-25 23:27:37 +0900

    :rotating_light: : pdf 관련 에러 주석처리

--- src/main/java/com/capstone03/goldenglobe/PdfList/FileStorageService.java
+++ src/main/java/com/capstone03/goldenglobe/PdfList/FileStorageService.java
@@ -1,32 +1,32 @@
-package com.capstone03.goldenglobe.PdfList;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-@Service
-public class FileStorageService {
-  @Value("${file.upload-dir}")
-  private String uploadDir;
-
-  public void storeFile(MultipartFile file) throws IOException {
-    Path uploadPath = Paths.get(uploadDir);
-
-    if (!Files.exists(uploadPath)) {
-      Files.createDirectories(uploadPath);
-    }
-
-    Path filePath = uploadPath.resolve(file.getOriginalFilename());
-    Files.copy(file.getInputStream(), filePath);
-  }
-
-  public void deleteFile(String filename) throws IOException {
-    Path filePath = Paths.get(uploadDir).resolve(filename);
-    Files.deleteIfExists(filePath);
-  }
-}
+//package com.capstone03.goldenglobe.PdfList;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//
+//@Service
+//public class FileStorageService {
+//  @Value("${file.upload-dir}")
+//  private String uploadDir;
+//
+//  public void storeFile(MultipartFile file) throws IOException {
+//    Path uploadPath = Paths.get(uploadDir);
+//
+//    if (!Files.exists(uploadPath)) {
+//      Files.createDirectories(uploadPath);
+//    }
+//
+//    Path filePath = uploadPath.resolve(file.getOriginalFilename());
+//    Files.copy(file.getInputStream(), filePath);
+//  }
+//
+//  public void deleteFile(String filename) throws IOException {
+//    Path filePath = Paths.get(uploadDir).resolve(filename);
+//    Files.deleteIfExists(filePath);
+//  }
+//}
