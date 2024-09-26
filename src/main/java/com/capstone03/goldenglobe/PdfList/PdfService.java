d63747ffc3405aaac683b231dc78a1059266059a
Jaeyeong Won <devlog.jae13@gmail.com>
2024-09-26 14:56:19 +0900

    :sparkles: : pdf 서버에 업로드되도록

---
+++ src/main/java/com/capstone03/goldenglobe/PdfList/PdfService.java
@@ -0,0 +1,70 @@
+package com.capstone03.goldenglobe.PdfList;
+
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.capstone03.goldenglobe.user.CustomUser;
+import com.capstone03.goldenglobe.user.User;
+import com.capstone03.goldenglobe.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PdfService {
+  @Value("${aws.s3.pdf-bucket}")
+  private String bucket;
+  private final AmazonS3 amazonS3;
+  private final UserRepository userRepository;
+
+  public String getPresignedUrl(Authentication auth) {
+    CustomUser customUser = (CustomUser) auth.getPrincipal();
+    String fileName = "pdfs/" + customUser.getId() + "_" + UUID.randomUUID().toString() + ".pdf";
+
+    // URL 만료 시간 설정
+    Date expiration = new Date();
+    expiration.setTime(expiration.getTime() + 1000 * 60 * 5); // 5분 후 만료
+
+    // Presigned URL 생성
+    GeneratePresignedUrlRequest generatePresignedUrlRequest =
+        new GeneratePresignedUrlRequest(bucket, fileName)
+            .withMethod(HttpMethod.PUT)
+            .withExpiration(expiration);
+
+    return amazonS3.generatePresignedUrl(generatePresignedUrlRequest).toString();
+  }
+
+  public String uploadPdf(Authentication auth, MultipartFile file) throws IOException {
+    CustomUser customUser = (CustomUser) auth.getPrincipal();
+    String fileName = "pdfs/" + customUser.getId() + "_" + UUID.randomUUID().toString() + ".pdf";
+
+    ObjectMetadata metadata = new ObjectMetadata();
+    metadata.setContentType(file.getContentType());
+    metadata.setContentLength(file.getSize());
+
+    // 파일 업로드
+    amazonS3.putObject(bucket, fileName, file.getInputStream(), metadata);
+
+    // 사용자의 PDF 경로를 업데이트하려면 아래 코드를 추가합니다.
+    Optional<User> user = userRepository.findById(customUser.getId());
+    if (user.isPresent()) {
+      // 예: user.get().setPdfPath(fileName);  // PDF 경로를 사용자 엔티티에 설정할 경우
+      userRepository.save(user.get());
+    } else {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+    }
+
+    return "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/" + fileName; // S3 URL 반환
+  }
+}
