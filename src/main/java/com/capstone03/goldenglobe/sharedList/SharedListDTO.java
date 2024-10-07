84ed86d2f6a8a707d77acaa3a779a6ff407986ff
tdddt <doto1116@ewhain.net>
2024-10-07 23:26:57 +0900

    :recycle: : 공유 API request 통일

--- src/main/java/com/capstone03/goldenglobe/sharedList/SharedListDTO.java
+++ src/main/java/com/capstone03/goldenglobe/sharedList/SharedListDTO.java
@@ -1,29 +1,11 @@
 package com.capstone03.goldenglobe.sharedList;
 
-import com.capstone03.goldenglobe.groupMemo.GroupMemo;
-import com.capstone03.goldenglobe.groupMemo.GroupMemoDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class SharedListDTO {
-
-    private Long shared_id;
-    private Long list_id;
-    private Long user_id;
-    private String user_nickname;
-    private String user_color;
-    private String user_profile;
-
-    public static SharedListDTO fromEntity(SharedList sharedList) {
-        return new SharedListDTO(
-                sharedList.getSharedId(),
-                sharedList.getList().getListId(),
-                sharedList.getUser().getUserId(),
-                sharedList.getUser().getNickname(),
-                sharedList.getUserColor(),
-                sharedList.getUser().getProfile()
-        );
-    }
+    private String cellPhone;
+    private String color;
 }
